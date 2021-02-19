fa272650d6abb7e937cc519ad4fe77cc5e1b24e6
Abdallah Abedraba <aabedraba@gmail.com>
2021-02-19 11:04:00 +0100

    Add error screenshot

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-// Add this to PrismaClient to enable logging
-// https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/logging
-export const prisma = new PrismaClient({ log: ["query"] });
